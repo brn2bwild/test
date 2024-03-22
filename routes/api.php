3b0f59678933fedf410ca17657669ef2b316f3b8
brn2bwild <brn_2_b_wild@hotmail.com>
2024-03-22 13:02:51 -0600

    Agregadas las configuraciones para las rutas API.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function(Request $request){
+	return response()->json([
+		'data' => 'esto es un json'
+	]);
+})->name('api.index');
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
